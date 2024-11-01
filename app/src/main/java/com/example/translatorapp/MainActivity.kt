f45ab180958fffe3eb27779d1c348c837e8cb94e
Niraviman-Singha <niraviman15-11689@diu.edu.bd>
2024-11-02 00:30:12 +0600

    Translator App Backend part done.

--- app/src/main/java/com/example/translatorapp/MainActivity.kt
+++ app/src/main/java/com/example/translatorapp/MainActivity.kt
@@ -2,10 +2,61 @@ package com.example.translatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.mlkit.common.model.DownloadConditions
+import com.google.mlkit.nl.translate.TranslateLanguage
+import com.google.mlkit.nl.translate.Translation
+import com.google.mlkit.nl.translate.Translator
+import com.google.mlkit.nl.translate.TranslatorOptions
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var translator:Translator
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        val translateButton = findViewById<Button>(R.id.translateButton)
+        val outputText = findViewById<TextView>(R.id.outputText)
+
+        // Create an English-German translator:
+        val options = TranslatorOptions.Builder()
+            .setSourceLanguage(TranslateLanguage.ENGLISH)
+            .setTargetLanguage(TranslateLanguage.BENGALI)
+            .build()
+        translator = Translation.getClient(options)
+
+        var conditions = DownloadConditions.Builder()
+            .requireWifi()
+            .build()
+        translator.downloadModelIfNeeded(conditions)
+            .addOnSuccessListener {
+                translateButton.setOnClickListener {
+                    val textToTranslate = inputEditText.text.toString()
+                    translateText(textToTranslate, outputText)
+                }
+            }
+            .addOnFailureListener { exception ->
+               outputText.text = "Model Download Failed!"
+            }
+    }
+
+    private fun translateText(inputText:String, outputTextView: TextView){
+
+        translator.translate(inputText)
+            .addOnSuccessListener { translatedText ->
+               outputTextView.text = translatedText
+            }
+            .addOnFailureListener { exception ->
+                outputTextView.text = "Translation Failed!"
+            }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        translator.close()
     }
 }
